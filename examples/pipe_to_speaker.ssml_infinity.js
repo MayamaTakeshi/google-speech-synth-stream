775bd049db56a2408fe076bc1302170e13f59285
MayamaTakeshi <mayamatakeshi@gmail.com>
2024-05-08 16:53:58 +0900

    Added examples/pipe_to_speaker.ssml_infinity.js

---
+++ examples/pipe_to_speaker.ssml_infinity.js
@@ -0,0 +1,45 @@
+const Speaker = require('speaker')
+const GSSS = require('../index.js')
+const au = require('@mayama/audio-utils')
+const fs = require('fs')
+
+const audioFormat = 1 // LINEAR16
+
+const format = {
+  audioFormat,
+  channels: 1,
+  sampleRate: 16000,
+}
+
+const config = {
+	work_dir: './tmp',
+}
+
+if (!fs.existsSync('./tmp')) {
+  fs.mkdirSync('./tmp')
+}
+
+const params = {
+  language: 'en-US',
+	voice: 'en-US-Standard-G',
+	text: '<speak>hello world<break time="1s"/>how are you?</speak>',
+  times: Infinity,
+}
+
+const opts = {
+  format,
+  config,
+  params,
+}
+
+const gs = new GSSS(opts)
+
+const speaker = new Speaker(format)
+
+// We need to write some initial silence to the speaker to avoid scratchyness/gaps
+const size = 320 * 64 
+console.log("writing initial silence to speaker", size)
+data = au.gen_silence(audioFormat, true, size)
+speaker.write(data)
+
+gs.pipe(speaker)
